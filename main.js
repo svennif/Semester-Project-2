5b1b10b00e600b4d9641533fe2efcd324f1f1899
svennif <33319198+svennif@users.noreply.github.com>
2020-09-06 18:15:37 +0200

    Commit 14

--- main.js
+++ main.js
@@ -15,14 +15,24 @@ function showObject(characterObject) {
 
     for (let i = 0; i < cardData.length; i++) {
         cardContainer.innerHTML += "<div class='card'>" +
-        "<div class='card-body'>" +
-        "<label class='card-content' for='" + cardData[i].Id + "'>" +
-        "<h2 class='card-title'>" + cardData[i].Name + "</h2>" +
-        "<p class='card-text'>" + cardData[i].PlayedBy + "</p>" +
-        "<p class='card-text'>" + cardData[i].Aliases[0] + "</p>" +
-        "</div>" +
-        "<input class='checkbox stretched-link' type='checkbox'" + "id='" + cardData[i].Id + "' value='" + cardData[i].Id + "'>" +
-        "</div>" +
-        "</label>" ;
+            "<input class='stretched-link checkbox' type='checkbox'" + "id='" + cardData[i].Id + "' value='" + cardData[i].Id + "'>" +
+            "<label class='card-content' for='" + cardData[i].Id + "'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title'>" + cardData[i].Name + "</h2>" +
+            "<p class='card-text'>" + cardData[i].PlayedBy + "</p>" +
+            "<p class='card-text'>" + cardData[i].Aliases[0] + "</p>" +
+            "</div>" +
+            "</div>" +
+            "</label>";
     }
+
+    $("input[type=checkbox]").change(function () {
+        var max = 2;
+        if ($("input[type=checkbox]:checked").length == max) {
+            $("input[type=checkbox]").attr('disabled', 'disabled');
+            $("input[type=checkbox]:checked").removeAttr('disabled');
+        } else {
+            $("input[type=checkbox]").removeAttr('disabled');
+        }
+    })
 }
