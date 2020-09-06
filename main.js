51fb07f11288c3e22fc27771c5f8c1b53bb2162c
svennif <33319198+svennif@users.noreply.github.com>
2020-09-06 19:56:08 +0200

    Commit 15

--- main.js
+++ main.js
@@ -16,11 +16,10 @@ function showObject(characterObject) {
     for (let i = 0; i < cardData.length; i++) {
         cardContainer.innerHTML += "<div class='card'>" +
             "<input class='stretched-link checkbox' type='checkbox'" + "id='" + cardData[i].Id + "' value='" + cardData[i].Id + "'>" +
-            "<label class='card-content' for='" + cardData[i].Id + "'>" +
-            "<div class='card-body'>" +
+            "<label class='card-body' for='" + cardData[i].Id + "'>" +
             "<h2 class='card-title'>" + cardData[i].Name + "</h2>" +
-            "<p class='card-text'>" + cardData[i].PlayedBy + "</p>" +
-            "<p class='card-text'>" + cardData[i].Aliases[0] + "</p>" +
+            "<p class='card-text'><b>Played by: </b>" + cardData[i].PlayedBy + "</p>" +
+            "<p class='card-text'><b>Known aliases: </b>" + cardData[i].Aliases[0] + "</p>" +
             "</div>" +
             "</div>" +
             "</label>";
@@ -31,8 +30,10 @@ function showObject(characterObject) {
         if ($("input[type=checkbox]:checked").length == max) {
             $("input[type=checkbox]").attr('disabled', 'disabled');
             $("input[type=checkbox]:checked").removeAttr('disabled');
+            $(".btn").css('visibility', 'visible');
         } else {
             $("input[type=checkbox]").removeAttr('disabled');
+            $(".btn").css('visibility', 'hidden');
         }
     })
 }
